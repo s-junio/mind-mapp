b021fde4212c238790dd0b05f1d147f6e19ba007
Christophe73770 <a73770@alunos.uminho.pt>
2021-03-10 15:51:18 +0000

    Button component creation.

--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Home.css";
+import MButton from "../../components/MButton/MButton";
+import {useHistory} from "react-router-dom";
 
 function Home() {
+  const history=useHistory();
+  const goToCreate=function(){
+    history.push("/mapp");
+  }
+  
   return (
     <div className="home-wrapper">
       <header className="Home-header">
         <div className="greeting">
           <h1>A new way to write down your thoughts.</h1>
         </div>
+        <MButton handleClick={goToCreate} label="START"></MButton>
       </header>
       <div className="footer"></div>
     </div>
